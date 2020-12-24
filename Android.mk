0d7783a34f24ba45da09d59dde9c86e3867610c9
2shady4u <jd16581556@gmail.com>
2020-12-24 14:18:34 +0100

    Add automated workflows for all platforms

---
+++ Android.mk
@@ -0,0 +1,36 @@
+# Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := godot-prebuilt
+ifeq ($(TARGET_ARCH_ABI),x86)
+    LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.debug.x86.a
+endif
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.debug.armv7.a
+endif
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+    LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.debug.arm64v8.a
+endif
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgdsqlite
+LOCAL_CPPFLAGS := -std=c++14
+LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_LDLIBS := -llog
+
+LOCAL_SRC_FILES := \
+src/gdsqlite.cpp \
+src/library.cpp \
+src/sqlite/sqlite3.c \
+
+LOCAL_C_INCLUDES := \
+godot-cpp/godot_headers \
+godot-cpp/include/ \
+godot-cpp/include/core \
+godot-cpp/include/gen \
+
+LOCAL_STATIC_LIBRARIES := godot-prebuilt
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ jni/Application.mk
@@ -0,0 +1,3 @@
+# Application.mk
+APP_STL:=c++_shared
+APP_ABI:=armeabi-v7a arm64-v8a x86
