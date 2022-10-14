9bce33e85571e20f78e5b999540d75357cec39f4
engineer-pearl <engineer.pearl0@gmail.com>
2022-10-13 19:59:35 -0700

    Finish adding functions

--- demo/addons/godot-sqlite/godot-sqlite-with-help.gd
+++ demo/addons/godot-sqlite/godot-sqlite-with-help.gd
@@ -157,91 +157,110 @@ func query(query_string : String) -> bool:
 func query_with_bindings(query_string : String, param_bindings : Array) -> bool:
 	return db.query_with_bindings(query_string, param_bindings)
 
-#- Boolean success = **create_table(** String table_name, Dictionary table_dictionary **)**
+
 ## Creates a table if it does not already exist [br]
 ## [br]
 ##  Each key/value pair of the `table_dictionary`-variable defines a column of the table. Each key defines the name of a column in the database, while the value is a dictionary that contains further column specifications. [br]
 ## [br]
 ##    **Required fields**: [br]
-# [br]
-#    - **"data_type"**: type of the column variable, following values are valid\*:
-#
-#        | value       | SQLite         | Godot          | [br]
-#        |:-----------:|:--------------:|:--------------:| [br]
-#        | int         | INTEGER        | TYPE_INT       | [br]
-#        | real        | REAL           | TYPE_REAL      | [br]
-#        | text        | TEXT           | TYPE_STRING    | [br]
-#        | char(?)\*\* | CHAR(?)\*\*    | TYPE_STRING    | [br]
-#        | blob        | BLOB           | TYPE_RAW_ARRAY | [br]
-#
-#        \* *Data types not found in this table throw an error and end up finalizing the current SQLite statement.*  
-#        \*\* *with the question mark being replaced by the maximum amount of characters*
-#
-#    **Optional fields**:
-#
-#    - **"not_null"** *(default = false)*: Is the NULL value an invalid value for this column?
-#
-#    - **"default"**: The default value of the column if not explicitly given.
-#
-#    - **"primary_key"** *(default = false)*: Is this the primary key of this table?  
-#    Evidently, only a single column can be set as the primary key.
-#
-#    - **"auto_increment"** *(default = false)*: Automatically increment this column when no explicit value is given. This auto-generated value will be one more (+1) than the largest value currently in use.
-#
-#        ***NOTE**: Auto-incrementing a column only works when this column is the primary key!*
-#
-#    - **"foreign_key"**: Enforce an "exist" relationship between tables by setting this variable to `foreign_table.foreign_column`. In other words, when adding an additional row, the column value should be an existing value as found in the column with name `foreign_column` of the table with name `foreign_table`.
-#
-#        ***NOTE**: Availability of foreign keys has to be enabled by setting the `foreign_keys`-variable to true BEFORE opening the database.*
-#
-#    **Example usage**:
-#
-#    ```gdscript
-#    # Add the row "id" to the table, which is an auto-incremented primary key.
-#    # When adding additional rows, this value can either by explicitely given or be unfilled.
-#    table_dictionary["id"] = {
-#        "data_type":"int", 
-#        "primary_key": true, 
-#        "auto_increment":true
-#    }
-#    ```
-#
-#    For more concrete usage examples see the `database.gd`-file as found in this repository's demo project.
-#
-#- Boolean success = **drop_table(** String table_name **)**
-#
-#- Boolean success = **insert_row(** String table_name, Dictionary row_dictionary **)**
-#
-#    Each key/value pair of the `row_dictionary`-variable defines the column values of a single row.  
-#
-#    Columns should adhere to the table schema as instantiated using the `table_dictionary`-variable and are required if their corresponding **"not_null"**-column value is set to `True`.
-#
-#- Boolean success = **insert_rows(** String table_name, Array row_array **)**
-#
-#- Array selected_rows = **select_rows(** String table_name, String query_conditions, Array selected_columns **)**
-#
-#- Boolean success = **update_rows(** String table_name, String query_conditions, Dictionary updated_row_dictionary **)**
-#
-#    With the `updated_row_dictionary`-variable adhering to the same table schema & conditions as the `row_dictionary`-variable defined previously.
-#
-#- Boolean success = **delete_rows(** String table_name, String query_conditions **)**
-#
-#- Boolean success = **import_from_json(** String import_path **)**
-#
-#    Drops all database tables and imports the database structure and content present inside of `import_path.json`.
-#
-#- Boolean success = **export_to_json(** String export_path **)**
-#
-#    Exports the database structure and content to `export_path.json` as a backup or for ease of editing.
-#
-#- Boolean success = **create_function(** String function_name, FuncRef function_reference, int number_of_arguments **)**
-#
-#    Bind a [scalar SQL function](https://www.sqlite.org/appfunc.html) to the database that can then be used in subsequent queries.
-#
+## [br]
+##    - **"data_type"**: type of the column variable, following values are valid\*:
+## [br]
+##        | value       | SQLite         | Godot          | [br]
+##        |:-----------:|:--------------:|:--------------:| [br]
+##        | int         | INTEGER        | TYPE_INT       | [br]
+##        | real        | REAL           | TYPE_REAL      | [br]
+##        | text        | TEXT           | TYPE_STRING    | [br]
+##        | char(?)\*\* | CHAR(?)\*\*    | TYPE_STRING    | [br]
+##        | blob        | BLOB           | TYPE_RAW_ARRAY | [br]
+## [br]
+## [br]       \* *Data types not found in this table throw an error and end up finalizing the current SQLite statement.*  
+## [br]       \*\* *with the question mark being replaced by the maximum amount of characters*
+## [br]
+## [br]    **Optional fields**:
+## [br]
+## [br]    - **"not_null"** *(default = false)*: Is the NULL value an invalid value for this column?
+## [br]
+## [br]    - **"default"**: The default value of the column if not explicitly given.
+## [br]
+## [br]    - **"primary_key"** *(default = false)*: Is this the primary key of this table?  
+## [br]    Evidently, only a single column can be set as the primary key.
+## [br]
+## [br]    - **"auto_increment"** *(default = false)*: Automatically increment this column when no explicit value is given. This auto-generated value will be one more (+1) than the largest value currently in use.
+## [br]
+## [br]        ***NOTE**: Auto-incrementing a column only works when this column is the primary key!*
+## [br]
+## [br]    - **"foreign_key"**: Enforce an "exist" relationship between tables by setting this variable to `foreign_table.foreign_column`. In other words, when adding an additional row, the column value should be an existing value as found in the column with name `foreign_column` of the table with name `foreign_table`.
+## [br]
+## [br]        ***NOTE**: Availability of foreign keys has to be enabled by setting the `foreign_keys`-variable to true BEFORE opening the database.*
+## [br]
+## [br]    **Example usage**:
+## [br]
+## [br]    ```gdscript
+## [br]    # Add the row "id" to the table, which is an auto-incremented primary key.
+## [br]    # When adding additional rows, this value can either by explicitely given or be unfilled.
+## [br]    table_dictionary["id"] = {
+## [br]        "data_type":"int", 
+## [br]        "primary_key": true, 
+## [br]        "auto_increment":true
+## [br]    }
+## [br]    ```
+## [br]
+## [br]    For more concrete usage examples see the `database.gd`-file as found in this repository's demo project.
+func create_table(table_name: String, table_dictionary: Dictionary) -> bool:
+	return db.create_table(table_name, table_dictionary)
+
+
+## Drops an existing table
+func drop_table(table_name: String) -> bool:
+	return db.drop_table(table_name)
+
+##    Each key/value pair of the `row_dictionary`-variable defines the column values of a single row.  
+##
+##    Columns should adhere to the table schema as instantiated using the `table_dictionary`-variable and are required if their corresponding **"not_null"**-column value is set to `True`.
+func insert_row(table_name: String, row_dictionary : Dictionary) -> bool:
+	return db.insert_row(table_name, row_dictionary)
+
+
+##Takes an array of dictionaries where each dictionary represents a row
+func insert_rows(table_name: String, row_array : Array) -> bool:
+	return db.insert_rows(table_name,row_array)
+
+
+func select_rows(table_name : String, query_conditions: String, selected_columns: Array) -> Array:
+	return db.select_rows(table_name,query_conditions,selected_columns)
+## Boolean success = **update_rows(** String table_name, String query_conditions, Dictionary updated_row_dictionary **)**
+##
+##    With the `updated_row_dictionary`-variable adhering to the same table schema & conditions as the `row_dictionary`-variable defined previously.
+func update_rows(table_name : String, query_conditions: String, updated_row_dictionary: Dictionary) -> bool:
+	return db.update_rows(table_name,query_conditions,updated_row_dictionary)
+
+
+## Boolean success = **delete_rows(** String table_name, String query_conditions **)**
+func delete_rows(table_name: String, query_conditions: String) -> bool:
+	return db.delete_rows()
+
+
+##   Drops all database tables and imports the database structure and content present inside of `import_path.json`.
+func import_from_json(import_path: String) -> bool:
+	return db.import_from_json(import_path)
+
+
+##    Exports the database structure and content to `export_path.json` as a backup or for ease of editing.
+func export_to_json(export_path : String) -> bool:
+	return db.export_to_json(export_path)
+
+
+##    Bind a [scalar SQL function](https://www.sqlite.org/appfunc.html) to the database that can then be used in subsequent queries.
+##    [br] Not usually a good idea. 
+##    [br] Creating function should only be seen as a measure of last resort and only be used when you perfectly know what you are doing. Be sure to first check out the available native list of scalar SQL applications that is already available in SQLite3.
+func create_function(function_name: String, function_reference: FuncRef, number_of_arguments: int) -> bool:
+	return db.create_function(function_name,function_reference,number_of_arguments)
 #- Integer mode = **get_autocommit()**
 #
-#    Get the current autocommit mode of the open database connection which can be used to check if there's any database transactions in progress, see [here](http://www.sqlite.org/c3ref/get_autocommit.html). A non-zero return value indicates that the database is in autocommit mode and thus has no active transaction.
-
+##    Get the current autocommit mode of the open database connection which can be used to check if there's any database transactions in progress, see [here](http://www.sqlite.org/c3ref/get_autocommit.html). A non-zero return value indicates that the database is in autocommit mode and thus has no active transaction.
+func get_autocommit() -> int:
+	return db.get_autocommit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -249,6 +268,3 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
