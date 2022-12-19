1536290bd740302c95edce51f6fd85ade33be6cf
Chris Ridenour <chrisridenour@gmail.com>
2022-12-19 14:12:03 -0500

    Update GDExtension code to match beta-9 renames from godot-cpp

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,6 +1,6 @@
 #include "register_types.h"
 
-#include <godot/gdnative_interface.h>
+#include <gdextension_interface.h>
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
@@ -28,7 +28,7 @@ extern "C" {
 
 // Initialization.
 
-GDNativeBool GDN_EXPORT sqlite_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
+GDExtensionBool GDE_EXPORT sqlite_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
     godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
     init_obj.register_initializer(initialize_sqlite_module);
