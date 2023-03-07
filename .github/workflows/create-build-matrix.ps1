3dd957c4748ed838ce09c17ed2673a674d9f34e3
Fee Gevaert <fee.gevaert@opengeohub.org>
2023-03-07 14:41:33 +0100

    plp

---
+++ .github/workflows/create-build-matrix.ps1
@@ -0,0 +1,18 @@
+$RawMatrix = Get-Content -Raw -Path .github/workflows/build_var.json | ConvertFrom-Json
+
+$Targets = $RawMatrix.targets
+$Jobs = $RawMatrix.jobs
+
+$Matrix = @()
+foreach ($job in $Jobs)
+{
+  foreach ($target in $Targets) 
+  {
+    $MatrixJob = $job.PsObject.Copy()
+    $MatrixJob | Add-Member -MemberType NoteProperty -Name 'target' -Value $target
+    $Matrix += $MatrixJob
+  }
+}
+
+Write-Host (ConvertTo-JSON -InputObject $Matrix)
+Write-Output "matrix=$(ConvertTo-JSON -InputObject $Matrix -Compress)" >> $env:GITHUB_OUTPUT
