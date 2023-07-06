7d95cda71e60ea89d28755f8b6f34569c1358d8f
2shady4u <jd16581556@gmail.com>
2023-07-06 20:47:55 +0200

    Update godot-cpp to 4.1-stable

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -28,8 +28,8 @@ extern "C" {
 
 // Initialization.
 
-GDExtensionBool GDE_EXPORT sqlite_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+GDExtensionBool GDE_EXPORT sqlite_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
     init_obj.register_initializer(initialize_sqlite_module);
     init_obj.register_terminator(uninitialize_sqlite_module);
