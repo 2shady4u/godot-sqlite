6aa125ad8ba6e834915fa2b530f9f8159237a37b
Dragos Daian <daian.dragos@yahoo.com>
2024-03-14 10:03:58 +0100

    Update resource_loader_sqlite.cpp

--- src/resource_loader_sqlite.cpp
+++ src/resource_loader_sqlite.cpp
@@ -1,5 +1,8 @@
 #include "resource_loader_sqlite.h"
 #include "resource_sqlite.h"
+#include <godot_cpp/classes/project_settings.hpp>
+
+using namespace godot;
 
 Variant ResourceFormatLoaderSQLite::_load(const String &p_path, const String &original_path, bool use_sub_threads, int32_t cache_mode) const {
 	Ref<SQLiteResource> sqlite_model = memnew(SQLiteResource);
@@ -7,17 +10,17 @@ Variant ResourceFormatLoaderSQLite::_load(const String &p_path, const String &or
 	return sqlite_model;
 }
 PackedStringArray ResourceFormatLoaderSQLite::_get_recognized_extensions() const {
-	PackedStringArray array;
-	array.push_back("db");
-	return array;
+	return ProjectSettings::get_singleton()->get("filesystem/import/sqlite/default_extension");
 }
 bool ResourceFormatLoaderSQLite::_handles_type(const StringName &type) const {
 	return ClassDB::is_parent_class(type, "SQLiteResource");
 }
 String ResourceFormatLoaderSQLite::_get_resource_type(const String &p_path) const {
 	String el = p_path.get_extension().to_lower();
-	if (el == "db") {
-		return "SQLiteResource";
+	for (String element : (PackedStringArray)ProjectSettings::get_singleton()->get("filesystem/import/sqlite/default_extension")) {
+		if (el == element) {
+			return "SQLiteResource";
+		}
 	}
 	return "";
 }
